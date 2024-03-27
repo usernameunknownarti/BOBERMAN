a6537bdd53b9004f19567262f833469c713ed9ef
LHenDria <lukaszwych123@gmail.com>
2024-03-27 20:19:27 +0100

    Dodano podstawy pod tworzenie map. Dodany entity_wall (rysuje kwadrat). Dodano ściany (jak na razie brak kolizji, przez ściany można przejść).

--- main.c
+++ main.c
@@ -11,22 +11,22 @@ struct pos {
     float x;
 };
 
+// Ta tablica to abominacja,
+// będzie trzeba napisać funkcję "draw_map()" która zczytuje wartości z pliku "map1.txt"
+// w folderze maps i wpisuje je do tej tablicy
+char map_data[7][10] = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+};
+
+GLuint mapDisplayList;
+
 struct pos character;
-
-float y = 0, x = 0;
-
-void init() {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // Wartosci do ustawienie, jak na razie nie ma roznicy co sie ustawi, ale moze to spowodowac bledy w przyszlosci.
-    gluOrtho2D(0.0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0);
-}
-
-void map() {
-    int map[7][10];
-    //github
-}
 
 void keyboard(int key, int miceX, int miceY) {
     switch (key) {
@@ -52,13 +52,49 @@ void keyboard(int key, int miceX, int miceY) {
     printf("%.3f, %.3f\n", character.x, character.y);
 }
 
+void entity_wall(float x, float y) {
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glRectf(x, y, x + 100, y + 100);
+}
+
+void draw_map() {
+    float sumx = 0.0f, sumy = 0.0f;
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (map_data[i][j] == 1) {
+                entity_wall(sumx, sumy);
+            }
+            sumx += 100.0f;
+            printf("x = %f\n", sumx);
+        }
+        sumx = 0.0f;
+        sumy += 100.0f;
+        printf("y = %f\n", sumy);
+    }
+    sumy = 0.0f;
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 0.0f, 1.0f);
-    glRectf(character.x, character.y, character.x + 100, character.y + 100);
+    glRectf(character.x + 100, character.y + 100, character.x + 200, character.y + 200);
+    glCallList(mapDisplayList);
     glFlush();
     glutPostRedisplay();
     glutSwapBuffers();
+}
+
+void init() {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // Wartosci do ustawienie, jak na razie nie ma roznicy co sie ustawi, ale moze to spowodowac bledy w przyszlosci.
+    gluOrtho2D(0.0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0);
+
+    mapDisplayList = glGenLists(1);
+    glNewList(mapDisplayList, GL_COMPILE);
+    draw_map();
+    glEndList();
 }
 
 int main(int argc, char **argv) {
@@ -68,10 +104,9 @@ int main(int argc, char **argv) {
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Boberman");
 
-    glutDisplayFunc(display);
-
     init();
 
+    glutDisplayFunc(display);
     glutSpecialFunc(keyboard);
 
     glutMainLoop();
