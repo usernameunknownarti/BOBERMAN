701cc4a558b9fae056a7f645ae36153ec77f6650
LHenDria <lukaszwych123@gmail.com>
2024-04-04 14:59:42 +0200

    Śmierć gracza jeżeli wejdzie w eksplozje. Zniszczalne przeszkody.

--- main.c
+++ main.c
@@ -35,11 +35,11 @@ enum moving_direction {
 char map_data[7][10] = {
         1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
         1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 1, 0, 0, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        0, 0, 2, 1, 0, 1, 0, 0, 0, 0,
+        0, 2, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 2, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 2, 0, 0, 0, 0, 0, 0, 0, 1
 };
 
 GLuint mapDisplayList;
@@ -49,39 +49,50 @@ bool pressed_F1 = false;
 void explosion() {
     // To cudo spawnuje bomby, nie wiem jak to zrobić lepiej ale działa, kiedyś się to poprawi :D
     glColor3f(1.0f, 0.0f, 0.0f);
-    if (map_data[bomb_position.y - 1][bomb_position.x] == 0) {
+    if (map_data[bomb_position.y - 1][bomb_position.x] != 1) {
         glRectf((float) bomb_position.x * 100, (float) (bomb_position.y - 1) * 100, (float) bomb_position.x * 100 + 100,
                 (float) (bomb_position.y - 1) * 100 + 100);
+        map_data[bomb_position.y - 1][bomb_position.x] = 3;
     }
-    if (map_data[bomb_position.y + 1][bomb_position.x] == 0) {
+    if (map_data[bomb_position.y + 1][bomb_position.x] != 1) {
         glRectf((float) bomb_position.x * 100, (float) (bomb_position.y + 1) * 100, (float) bomb_position.x * 100 + 100,
                 (float) (bomb_position.y + 1) * 100 + 100);
+        map_data[bomb_position.y + 1][bomb_position.x] = 3;
     }
-    if (map_data[bomb_position.y ][bomb_position.x] == 0) {
+    if (map_data[bomb_position.y][bomb_position.x] != 1) {
         glRectf((float) bomb_position.x * 100, (float) bomb_position.y * 100, (float) bomb_position.x * 100 + 100,
                 (float) bomb_position.y * 100 + 100);
+        map_data[bomb_position.y + 1][bomb_position.x] = 3;
     }
-    if (map_data[bomb_position.y][bomb_position.x - 1] == 0) {
+    if (map_data[bomb_position.y][bomb_position.x - 1] != 1) {
         glRectf((float) (bomb_position.x - 1) * 100, (float) bomb_position.y * 100,
                 (float) (bomb_position.x - 1) * 100 + 100,
                 (float) bomb_position.y * 100 + 100);
+        map_data[bomb_position.y][bomb_position.x - 1] = 3;
     }
-    if (map_data[bomb_position.y][bomb_position.x + 1] == 0) {
+    if (map_data[bomb_position.y][bomb_position.x + 1] != 1) {
         glRectf((float) (bomb_position.x + 1) * 100, (float) bomb_position.y * 100,
                 (float) (bomb_position.x + 1) * 100 + 100,
                 (float) bomb_position.y * 100 + 100);
+        map_data[bomb_position.y][bomb_position.x + 1] = 3;
+    }
+}
+
+void death_detection() {
+    int char_x = (int) truncf(character.x / 100);
+    int char_y = (int) truncf(character.y / 100);
+    if (map_data[char_x][char_y] == 3) {
+        exit(0);
     }
 }
 
 void place_bomb() {
     glColor3f(0.5f, 0.5f, 0.5f);
-    glRectf(bomb_position.x * 100, bomb_position.y * 100, bomb_position.x * 100 + 100,
-            bomb_position.y * 100 + 100);
-    printf("%llu\n", time(NULL));
+    glRectf((float)bomb_position.x * 100, (float)bomb_position.y * 100, (float)bomb_position.x * 100 + 100,
+            (float)bomb_position.y * 100 + 100);
     if (time(NULL) == bomb_timer) {
         want_to_bomb = false;
         explosion_allowed = true;
-        printf("despawned bomb\n");
         printf("%llu", bomb_timer);
     }
 }
@@ -96,7 +107,7 @@ bool hitbox_detection() {
 
     for (int i = char_y - 1; i <= char_y + 1; i++) {
         for (int j = char_x - 1; j <= char_x + 1; j++) {
-            if (map_data[i][j] == 1) {
+            if (map_data[i][j] == 1 || map_data[i][j] == 2) {
                 x1 = (float) j * 100.0f;
                 y1 = (float) i * 100.0f;
                 if (character.x + 50.0f <= x1 || character.x - 50.0f >= x1 + 100) {
@@ -195,6 +206,11 @@ void entity_wall(float x, float y) {
     glRectf(x, y, x + 100, y + 100);
 }
 
+void entity_crate(float x, float y) {
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glRectf(x, y, x + 100, y + 100);
+}
+
 void draw_map() {
     float sumx = 0.0f, sumy = 0.0f;
     for (int i = 0; i < 7; i++) {
@@ -203,11 +219,23 @@ void draw_map() {
                 entity_wall(sumx, sumy);
             }
             sumx += 100.0f;
-            printf("x = %f\n", sumx);
         }
         sumx = 0.0f;
         sumy += 100.0f;
-        printf("y = %f\n", sumy);
+    }
+}
+
+void draw_crates() {
+    float sumx = 0.0f, sumy = 0.0f;
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (map_data[i][j] == 2) {
+                entity_crate(sumx, sumy);
+            }
+            sumx += 100.0f;
+        }
+        sumx = 0.0f;
+        sumy += 100.0f;
     }
 }
 
@@ -215,16 +243,22 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     if (want_to_bomb) {
         place_bomb();
-    } else if(explosion_allowed == true) {
-        if(time(NULL) < bomb_timer+1) {
+    } else if (explosion_allowed == true) {
+        if (time(NULL) < bomb_timer + 1) {
             explosion();
         } else {
-            printf("you twat");
             pressed_F1 = false;
             bomb_timer = 0;
             explosion_allowed = false;
+            map_data[bomb_position.y - 1][bomb_position.x] = 0;
+            map_data[bomb_position.y + 1][bomb_position.x] = 0;
+            map_data[bomb_position.y][bomb_position.x] = 0;
+            map_data[bomb_position.y][bomb_position.x - 1] = 0;
+            map_data[bomb_position.y][bomb_position.x + 1] = 0;
         }
     }
+    death_detection();
+    draw_crates();
     glColor3f(1.0f, 0.0f, 1.0f);
     glRectf(character.x - 50, character.y - 50, character.x + 50, character.y + 50);
     update_movement();
